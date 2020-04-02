c5e873e507398961b43f6391bacd89a5284e8e2b
zhang Wenming <137908461@qq.com>
2020-04-03 00:36:08 +0800

    bootstrap

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ##论坛
 
 ##资料
-https://spring.io/guides
-https://spring.io/guides/gs/serving-web-content/
-
+[Spring 文档](https://spring.io/guides)
+[Spring Web](https://spring.io/guides/gs/serving-web-content)
+[Bootstrap](https://v3.bootcss.com/getting-started/)
+[Github OAuth](https://developer.github.com/apps/)
 ##工具
--- src/main/java/zwm/forum/community/community/controller/IndexController.java
+++ src/main/java/zwm/forum/community/community/controller/IndexController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloCtroller {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String greeting(@RequestParam("name") String name, Model model){
-            model.addAttribute("name",name);
-            return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
+    /*public String greeting(@RequestParam("name") String name, Model model){
+            model.addAttribute("name",name);
+            return "index";
+    }*/
 }
